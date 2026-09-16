41ee4ed369f55f8874b0f24770cfd0070895573e
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2026-09-16 22:27:47 +0200

    fix: move persisted reasoning validation into api package

--- api/app/clients/BaseClient.js
+++ api/app/clients/BaseClient.js
@@ -30,6 +30,7 @@ const {
   seedTurnConversation,
   needsRetentionConversation,
   getConversationWriteContext,
+  resolvePersistedReasoningOverride,
 } = require('@librechat/api');
 const {
   Constants,
@@ -47,7 +48,6 @@ const {
   stripReasoningLabelMetadata,
   resolveTurnLLMDeliveryPath,
   resolveUseResponsesApi,
-  reasoningOverrideSchema,
 } = require('librechat-data-provider');
 const { getStrategyFunctions } = require('~/server/services/Files/strategies');
 const { logViolation } = require('~/cache');
@@ -608,12 +608,15 @@ class BaseClient {
       if (referencedQuotes != null) {
         userMessage.quotes = referencedQuotes;
       }
-      const reasoningOverride = reasoningOverrideSchema.safeParse(
-        this.options.req?.body?.reasoningOverride,
-      );
-      if (reasoningOverride.success) {
-        userMessage.reasoningOverride = reasoningOverride.data;
-      }
+    }
+
+    const persistedReasoningOverride = resolvePersistedReasoningOverride({
+      rawReasoningOverride: this.options.req?.body?.reasoningOverride,
+      isEdited: opts.isEdited,
+      isCompaction: opts.isCompaction,
+    });
+    if (persistedReasoningOverride !== undefined) {
+      userMessage.reasoningOverride = persistedReasoningOverride;
     }
 
     if (typeof opts?.getReqData === 'function') {
